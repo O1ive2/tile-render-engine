bc2a5618b3ff9f33d35b2141e830a3bcb1bcd047
wanhaoyi <2799274183@qq.com>
2024-12-18 10:57:14 +0800

    chore: render init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef, useMemo } from "react";
 import { TileMapProps } from "./interface";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { init } from "./test";
 
 const TileMap: React.FC<TileMapProps> = ({
   tileData,
@@ -20,19 +20,19 @@ const TileMap: React.FC<TileMapProps> = ({
     const sideLen = Math.floor(Math.sqrt(tileData.length));
 
     return tileData.map((item) => {
-      const { base64, index } = item;
+      const { blockBase64Str, index } = item;
       const img = new Image();
-      img.src = `data:img/png;base64,${base64}`;
-      const x = tileWidth*(index % sideLen);
-      const y = tileHeight*(Math.floor(index/sideLen));
+      img.src = `data:img/png;base64,${blockBase64Str}`;
+      const x = tileWidth * (index % sideLen);
+      const y = tileHeight * Math.floor(index / sideLen);
 
       return { img, x, y };
     });
   }, [tileData]);
 
-  // 加载瓦片并渲染
   const drawTiles = (context: CanvasRenderingContext2D) => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // 清空画布
+    console.log("draw");
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
     imgCache.forEach((item) => {
       const { x, y, img } = item;
@@ -132,8 +132,7 @@ const TileMap: React.FC<TileMapProps> = ({
     const context = canvas?.getContext("2d");
 
     if (context) {
-      console.log("draw");
-      drawTiles(context);
+      requestAnimationFrame(() => drawTiles(context));
     }
   }, [tileData, zoomLevel, viewport]);
 
@@ -149,46 +148,6 @@ const TileMap: React.FC<TileMapProps> = ({
   );
 };
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-const data = [
-  {
-    base64: "iVBORw0KGgoAAAANSUhEUgAAAIMAAABICAIAAABa0NFGAAAABmJLR0QA/wD/AP+gvaeTAAAB1klEQVR4nO3cz00CQRiH4W+MBRgacAvwwNEDyQ4nb7YABxugApcObGELMLGEoROOXq1gPbiRPxnIwO7AT/d9wgEXNCOvLDOTja5pGoOAm2sPAC1KqKCECkqooISK21O/wXtbrXKMZOjcqbNY54x5bw6cnVRQQgUlVFBCxclzpz1MpfrSde7EVKovnJ1UUEIFJVRQQgUlVFBCBSVUpK7sQrAQ2vtVtfPQ3pfXEdbVqq7Kmfni2kM5U+p7YjrNOgwkr7F/19KssTPhc0IFJVRQQgUlVFBCBSVUxEqEtYX1pQcyeLE19rR+sef3u2L72Gh0mfEMV6xEeT//+nyY7RxbLDa7Gux25BArEeYTs8nusfF4s++EHLiiQAVzJxWUUEEJFZRQQQkVlFBBCRWUUBFbY7vlmz1+lE/bx4rCiqK9z27Hebw37w8+mvqeqOteBjNox6+P4dqOyzn+WvE5oYISKiihghIqKKGCEioooYISKrr+j4IIt+z/Z4prXlOeVZbm3MFHM5Sw1JH9E8l/eccvjuHspIISKiihghIqKKGCEioooYISKiihghIqupb42UvZvlXm+xjYn1GZ33sFzrx1vLYj9tTl4Pad+vh9OTupoIQKSqighIpvctduWGTJHVUAAAAASUVORK5CYII=",
-    index: 0
-  },
-  {
-    base64: "iVBORw0KGgoAAAANSUhEUgAAAIMAAABICAIAAABa0NFGAAAABmJLR0QA/wD/AP+gvaeTAAABcElEQVR4nO3cQW6DMBQA0f8R9zI5Wc3NuBndVFVXtSEOmZZ5yi4RchgwyEHJfd9DANO7B6AvlqCwBIUlKCxBkRHeO71cKbFtjc/M3sReILP9GWcnCktQWILCEhRzz8VE5/TcMn3z3umFDh3lzk4UlqCwBIUlKOZhW8p12Kb+iv1j4MbGlYjBI6MbfeQ5O1HMtf729rLEslwykNtrnBOPxzXDUDTOifV+l+F38TpBYQkKS1BYgsISFJagaJQoJTK7XjWWSwb8bzXWnfp//IvcIspTY7m36cC+1itNrmdAeMWmsASFJSgsQWEJCktQWILCEhRT/8qS604/1Via+6QcWf3JYf8Wkevtnnfq/r6Z0dzNzk4UlqCwBIUlKCxBYQkKS1BYgsISFJagmIYsOt1w3Wm4cf9R4PNOz3F2orAEhSUoLEFhCQpLUFiCwhIUlqCwBIXrTif1PO906MEn151OqrHVfeT3dXaisASFJSgsQfEJ3h4y6zeyPZkAAAAASUVORK5CYII=",
-      index: 1
-  },
-  {
-    base64: "iVBORw0KGgoAAAANSUhEUgAAAIMAAABICAIAAABa0NFGAAAABmJLR0QA/wD/AP+gvaeTAAABXklEQVR4nO3cQQ6CMBQA0dZ4L3szy8nUk+HCxBXU0lIywXlhYYQQcIKQvyDO8xwqxBg+G34/rG86hfles8+T2Ol8L/270C4sQWEJCktQWIJic4nbLcRYWnJIA46TK4dU/kFql61PsRWb+hTbwn8nCktQWILCEhSWoLAEhSUoLEFhCQpLUNSW+Dlucu500NxpA+dOTfx3orAEhSUoLEFhCQpLUFiCwhIUlqBYKpGfIT+PPpC/t1RieuUpNs9PnDvtN3f6XBA5NR6ac6cm14Xvmhuog3dsCktQWILCEhSWoLAEhSUoLEHh3InCuVMv504Yzp1Oxjs2hSUoLEFhCQpLUFiCwhIUlqBw7kTh3KlX/dwppdJ+nDt1qz7fWHxHlnMnCu/YFJagsASFJSgsQWEJiqWnWA2T8+oqr4njPB6ltV4Tx0mpNPAYUyJOQ3Z7agPe76Qm3icoLEFhCQpLULwBqUFae59ZdwgAAAAASUVORK5CYII=",
-      index: 2
-  },
-  {
-    base64: "iVBORw0KGgoAAAANSUhEUgAAAIMAAABICAIAAABa0NFGAAAABmJLR0QA/wD/AP+gvaeTAAABV0lEQVR4nO3cwXGDMBBA0V2GQtKJ1UlKQZTiSix34k6Uc/ApoBk+8X/jEweN4I/B3gPZe48hco2+jFnqEkaf7zRwLR1hCQpLUFiCwhIUU2YM+dQoZ5/Ltc2jfsRGtojboLU+kXcnCktQWILCEhSWoLAEhSUoLEFhCQpLnKG2qG1zzLnTTjXK/mu1ZqzPzYLOnXaq0Wrfe761v1+r+eiOtEMt78d8TlBYgsISFJagsASFJSgsQWEJCktQOHeicO5E4d2JwhIUlqCwBIUlKCxBYQkKS1BYgsISFHMp0dqgxXIdtNAnyohh74vQEd6dKCxBYQkKS1BYgsISFJagsARFRvTluq/va6/6vNfbd5Svs7fyB2W9l3htXiM4PR5n7Ue/pLMOCJ8TFJagsASFJSgsQWEJCktQWIIil+XK/+z+07TjrN1ow2kHhd8JCktQWILCEhSWoLAExQ9ApkFzinEERwAAAABJRU5ErkJggg==",
-      index: 3
-  }
-];
-
-const onTileClick = (coords: { x: number; y: number }) =>
-  console.log("click", coords);
-const handlewheel = (newViewport: number) =>
-  console.log("222212", newViewport);
-
-root.render(
-  <React.StrictMode>
-    <TileMap
-      tileData={data}
-      onTileClick={onTileClick}
-      handlewheel={handlewheel}
-      tileWidth={131}
-      tileHeight={72}
-      width={1000}
-      height={1000}
-    />
-  </React.StrictMode>
-);
+init();
 
 export default TileMap;
--- src/interface.ts
+++ src/interface.ts
@@ -2,7 +2,9 @@ export interface TileMapProps {
   tileData: TileDataProps[];
   onTileClick?: (clickProps: ClickProps) => void;
   handlewheel?: (newViewport: number) => void;
-  onDragMove?: (from: Location, to: Location) => void;
+  onDragStart?: (startX: number, startY: number) => void;
+  onDragMove?: (moveX: number, moveY: number) => void;
+  onDragEnd?: (endX: number, endY: number) => void;
   tileWidth: number;
   tileHeight: number;
   width?: number;
@@ -10,13 +12,22 @@ export interface TileMapProps {
 }
 
 export interface TileDataProps {
-  base64: string;
+  blockBase64Str: string;
   index: number;
 }
 
 export interface Location {
   x: number;
   y: number;
+}
+
+export interface TileMapEventInfo {
+  type?: "Wheel" | "Click" | "DragMove";
+  viewPort?: {
+    x?: number;
+    y?: number;
+  };
+  zoomLevel?: number;
 }
 
 export interface ClickProps {
